0d758d8ec92a6e09138e037179c4af99b460ee29
Kazukai • 剣 <136913040+TeamKazukai@users.noreply.github.com>
2023-09-16 11:25:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/1238muj/THALAPATHY-FILTER-BOT.git /THALAPATHY-FILTER-BOT 
+  git clone https://github.com/TeamKazukai/Lucy.git /Lucy 
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /THALAPATHY-FILTER-BOT 
 fi
-cd /THALAPATHY-FILTER-BOT 
+cd /Lucy
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
